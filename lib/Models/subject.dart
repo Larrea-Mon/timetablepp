f9fb6d3ba2d6fc26f4af24e6eefe78fb03ab0bd3
larrea-montoya <julia.larrea.montoya@protonmail.com>
2024-07-05 12:50:28 +0200

    Colorines

--- lib/Models/subject.dart
+++ lib/Models/subject.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:objectbox/objectbox.dart';
 
-
-
 @Entity()
 class Subject {
   @Id()
@@ -23,31 +21,47 @@ class Subject {
   }
 }
 
-Color colorToColor(String color){
-  if (color == 'cyan') {
+Color colorToColor(String color) {
+  if (color == 'sky') {
     return Colors.blue[200]!;
-  } else if (color == 'blue'){ 
+  } else if (color == 'river') {
     return Colors.blue[700]!;
-  } else if(color == 'grass'){
+  } else if (color == 'grass') {
     return Colors.green[200]!;
-  } else if (color == 'forest'){
+  } else if (color == 'forest') {
     return Colors.green[700]!;
+  } else if (color == 'sun') {
+    return Colors.yellow[600]!;
+  } else if (color == 'orange') {
+    return Colors.orange[600]!;
+  }else if (color == 'pink') {
+    return Colors.pink[300]!;
+  }else if (color == 'apple') {
+    return Colors.red[600]!;
+  }else if (color == 'lila') {
+    return Colors.purple[400]!;
+  }else if (color == 'purple') {
+    return Colors.purple[600]!;
+  }else if (color == 'chocolate') {
+    return Colors.brown[400]!;
+  } else if(color == 'wood') {
+    return Colors.brown[700]!;
   } else {
     return Colors.black;
   }
 }
 
 enum SubjectColors {
-  cyan,
-  blue,
+  sky,
+  river,
   grass,
   forest,
-  yellow,
+  sun,
   orange,
   pink,
-  red,
+  apple,
   lila,
   purple,
   chocolate,
-  brown
+  wood
 }
